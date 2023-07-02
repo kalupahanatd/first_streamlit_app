72dcd8c8a5cebd97d1c54e5bc7759a1a54bbf906
kalupahanatd <67464246+kalupahanatd@users.noreply.github.com>
2023-07-02 15:11:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from urllib.error import URLError
 
 
 
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
