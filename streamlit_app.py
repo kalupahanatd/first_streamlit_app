e6613bb7e37e1ec4dd64e70180acd1ecb560906c
kalupahanatd <67464246+kalupahanatd@users.noreply.github.com>
2023-07-02 14:37:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.stop()
+
 import snowflake.connector
 from urllib.error import URLError
 
@@ -48,6 +48,8 @@ import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 streamlit.write('User Entered', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+streamlit.stop()
 
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
